a2bca2ac308cbd04cc84bf06dabcb815a6fe9a63
soydevcag <ariasgonzalezcamilo@gmail.com>
2016-05-04 17:28:41 -0500

    Message Register

--- app/src/main/java/com/training/lequar/trainingfit/Register.java
+++ app/src/main/java/com/training/lequar/trainingfit/Register.java
@@ -20,10 +20,15 @@ import com.training.lequar.trainingfit.Model.Utilities.Urls;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.content.Context;
+import android.os.CountDownTimer;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Register extends AppCompatActivity {
     Urls urls = new Urls();
     Context context = this;
+    String message = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,19 +49,49 @@ public class Register extends AppCompatActivity {
                 final EditText phone = (EditText) findViewById(R.id.phone);
                 final EditText email = (EditText) findViewById(R.id.email);
                 final EditText pass = (EditText) findViewById(R.id.pass);
+
                 String url = urls.getRegisterUser();
                 StringRequest postRequest = new StringRequest(Request.Method.POST, url,
                         new Response.Listener<String>()
                         {
                             @Override
                             public void onResponse(String response) {
-                                // response
-                                Log.d("Response", response);
-                                AlertDialog alertDialog = new AlertDialog.Builder(context).create();
-                                alertDialog.setTitle("Mensaje");
-                                alertDialog.setMessage(response);
-                                alertDialog.setIcon(R.drawable.danger);
-                                alertDialog.show();
+                                try {
+                                    JSONObject jsonObject= new JSONObject(response.toString());
+                                    JSONObject objt = jsonObject.getJSONObject("content");
+                                    message = objt.getString("status");
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                final AlertDialog.Builder dialog = new AlertDialog.Builder(context)
+                                        .setTitle("Genial").setMessage(
+                                                message);
+                                dialog.setPositiveButton("Ok",
+                                        new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int whichButton) {
+                                                dialog.dismiss();
+
+                                            }
+                                        });
+                                final AlertDialog alert = dialog.create();
+                                alert.show();
+
+                                new CountDownTimer(3000, 1000) {
+                                    @Override
+                                    public void onTick(long millisUntilFinished) {
+                                        name.setText("");
+                                        lastName.setText("");
+                                        pass.setText("");
+                                        email.setText("");
+                                        phone.setText("");
+                                    }
+
+                                    @Override
+                                    public void onFinish() {
+                                        alert.dismiss();
+                                    }
+                                }.start();
                             }
                         },
                         new Response.ErrorListener()
